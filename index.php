9c24165ac982d1727b7ef83b288df6d83f492164
AsadBaig <webdev@awesomeanalytics.in>
2024-08-08 20:54:44 +0530

    Database and class

--- index.php
+++ index.php
@@ -1,35 +1,12 @@
 <?php
 
 require 'functions.php';
-
+require 'Database.php';
 // require 'router.php';
 
-// class Person
-// {
-//   public $name;
-//   public $age;
+$db = new Database();
 
-//   public function breathe()
-//   {
-//     echo $this->name . ' is breathing!';
-//   }
-// }
-
-// $person = new Person();
-
-// $person->name = 'Asad Baig';
-// $person->age = 27;
-
-// $person->breathe();
-
-$dsn = "mysql:host=localhost;port=3306;dbname=tailwind_php;user=root;charset=utf8mb4";
-
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
   echo "<li>" . $post['title'] . "</li>";

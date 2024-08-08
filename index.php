cd5b78662da31218aac89422beba7a35c29c7cd1
AsadBaig <webdev@awesomeanalytics.in>
2024-08-08 20:07:36 +0530

    Class and objects

--- index.php
+++ index.php
@@ -2,4 +2,22 @@
 
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
+
+class Person
+{
+  public $name;
+  public $age;
+
+  public function breathe()
+  {
+    echo $this->name . ' is breathing!';
+  }
+}
+
+$person = new Person();
+
+$person->name = 'Asad Baig';
+$person->age = 27;
+
+$person->breathe();
